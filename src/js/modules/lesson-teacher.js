6ad655fc62b995e7b6728b35e36d6d6be0a7b027
qqcrashtestqq <90323345+qqcrashtestqq@users.noreply.github.com>
2023-10-26 12:59:51 +0300

    edits and create dz teacher pages

--- src/js/modules/lesson-teacher.js
+++ src/js/modules/lesson-teacher.js
@@ -1,24 +1,43 @@
-function initializeTabs() {
-    const tabs = document.querySelectorAll(".tab");
-    const tabContents = document.querySelectorAll(".tab-content");
-    if(!tabs.length) return
-    tabs.forEach((tab) => {
-      tab.addEventListener("click", () => {
-        tabs.forEach((t) => t.classList.remove("active"));
-        tab.classList.add("active");
-  
-        tabContents.forEach((content) => content.classList.remove("active"));
-        const tabId = tab.getAttribute("data-tab");
-        document.getElementById(tabId).classList.add("active");
-      });
-    });
-  
-    // По умолчанию активируем первый таб
-    tabs[0].click();
-  }
-  
-  // Вызов функции для инициализации табов
-  initializeTabs();
+// function initializeTabs(tabClass, contentClass) {
+//   const tabs = document.querySelectorAll(`.${tabClass}`)
+//   const tabContents = document.querySelectorAll(`.${contentClass}`)
+//   if (!tabs.length || !tabContents.length) return
 
+//   tabs.forEach(tab => {
+//     tab.addEventListener('click', () => {
+//       tabs.forEach(t => t.classList.remove('active'))
+//       tab.classList.add('active')
+
+//       tabContents.forEach(content => content.classList.remove('active'))
+//       const tabId = tab.getAttribute('data-tab')
+//       document.getElementById(tabId).classList.add('active')
+//     })
+//   })
+//   tabs[0].click()
+// }
+
+
+// initializeTabs('tab', 'tab-content')
+
+function initializeTabs() {
+  const bindTabs = (tabClass, contentClass) => {
+    const tabs = document.querySelectorAll(`.${tabClass}`)
+    const tabContents = document.querySelectorAll(`.${contentClass}`)
+
+    if (!tabs.length || !tabContents.length) return
+
+    tabs.forEach((tab, index) => {
+      tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'))
+        tab.classList.add('active')
+
+        tabContents.forEach(c => c.classList.remove('active'))
+        tabContents[index].classList.add('active')
+      })
+    })
+    tabs[0].click()
+  }
+  bindTabs('tab', 'tab-content')
+}
 
 export default initializeTabs
